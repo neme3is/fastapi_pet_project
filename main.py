4ff7bb06ffaa51b641c2e836aef8015a6ac03af1
Aleksandr Ivanov <ivalex70@gmail.com>
2024-02-10 17:48:04 +0300

    add add_user exception handler

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from utils.orm_models.user_model import User
 
@@ -23,6 +22,8 @@ async def test_2():
 
 @app.post("/add-user/{user_name}")
 async def add_user(user_name):
+    if len(User.get_user_by_name(user_name)) > 0:
+        raise HTTPException(status_code=409, detail="This user already registered")
     User.add_user(user_name)
     user_id = User.get_user_by_name(user_name)[0].id
     return f"User: {user_name} added, id: {user_id}"
